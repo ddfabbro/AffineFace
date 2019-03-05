b4369d0d78a033c236c993e7fba93755b82c73bf
Davi <fabda01@gmail.com>
2019-03-05 13:02:46 -0300

    Add parallel for loops

--- nonrigid/nonrigid.py
+++ nonrigid/nonrigid.py
@@ -1,17 +1,20 @@
 import numpy as np
-import skimage.transform as tf
-from skimage.draw import polygon
+from skimage import transform, draw
 from scipy.spatial import Delaunay
+from joblib import Parallel, delayed
 
-def nonrigid(img, landmarks, template):
+def nonrigid(img, landmarks, template, n_jobs=-1):
+    def warp_triangles(src, dst):
+        X, Y = draw.polygon(dst[:,0], dst[:,1])
+        tform = transform.estimate_transform("affine", dst, src)
+        warped_img[Y, X] = transform.warp(img, tform)[Y, X]
+        
     simplices = Delaunay(template).simplices
     template_vertices = template[simplices]
     img_vertices = landmarks[simplices]
     
     warped_img = np.zeros(img.shape)
-    for src, dst in zip(img_vertices, template_vertices):
-        X, Y = polygon(dst[:,0], dst[:,1])
-        tform = tf.estimate_transform('affine', dst, src)
-        warped_img[Y, X] = tf.warp(img, tform)[Y, X]
+    Parallel(n_jobs=n_jobs, require="sharedmem")(delayed(warp_triangles)
+        (src, dst) for src, dst in zip(img_vertices, template_vertices))
         
     return warped_img
